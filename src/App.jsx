204959df72a3e694f44d34be07c5e3faaa48cb50
Marcin Czenko <marcin.czenko@pm.me>
2022-11-06 22:54:03 +0000

    Update App.jsx - shows some svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-const App = () => (
-  <div className='w-screen h-screen flex flex-col justify-center items-center bg-blue-700 text-white text-4xl'>
-    <p>vite-tailwind-starter</p>
-    <div className='bg-white p-1'>
-      <p className='text-xl text-blue-700'>with yarn/bun</p>
+import reactLogo, { ReactComponent as Logo } from '~/assets/react.svg'
+import { ReactComponent as DiscordLogo } from '~/assets/discord-logo-black.svg'
+
+const App = () => {
+  console.log('reactLogo=', reactLogo)
+  return (
+    <div className='h-screen w-screen flex-col items-center justify-center bg-blue-700 text-4xl text-white
+    flex'>
+      <img src={reactLogo} alt='React SVG' />
+      Test 2
+      <Logo />
+      <div className='w-[200px] border border-black'>
+        <DiscordLogo width={200} />
+      </div>
     </div>
-    <p className='text-base'>with eslint</p>
-    <p className='text-xl'>
-      with local{' '}
-      <span className='font-roboto-mono text-3xl font-thin'>Roboto Mono</span>{' '}
-      font
-    </p>
-    <p className='text-xl'>
-      and{' '}
-      <span className='font-roboto-mono italic text-3xl font-thin'>
-        Roboto Mono Italic
-      </span>
-    </p>
-  </div>
-)
+  )
+}
 
 export default App
