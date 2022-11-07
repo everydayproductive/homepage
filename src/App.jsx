580d09ce87a4f6a4ba0ef4928d3eef3124c042fa
Marcin Czenko <marcin.czenko@pm.me>
2022-11-07 02:42:38 +0100

    updates discord and slack links

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ const App = () => {
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center bg-[#459ED0] text-4xl text-white'>
       <EPLogo width={300} />
-      <a className='mt-5' href='https://discord.everydayproductive.com'>
+      <a
+        className='mt-5'
+        href='http://discord.everydayproductive.com'
+        target='_blank'
+        rel='noreferrer'
+      >
         <DiscordLogoWhite width={200} />
       </a>
-      <a className='mt-5' href='https://slack.everydayproductive.com'>
+      <a
+        className='mt-5'
+        href='http://slack.everydayproductive.com'
+        target='_blank'
+        rel='noreferrer'
+      >
         <SlackLogoWhite width={200} />
       </a>
     </div>
