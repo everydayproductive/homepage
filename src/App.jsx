583646de513a7854e0cd3e8913ae5b5cf303742e
Marcin Czenko <marcin.czenko@pm.me>
2022-11-07 02:02:31 +0100

    adds discord and slack links

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ const App = () => {
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center bg-[#459ED0] text-4xl text-white'>
       <EPLogo width={300} />
-      <DiscordLogoWhite width={200} />
-      <SlackLogoWhite width={200} />
+      <a className='mt-5' href='https://discord.everydayproductive.com'>
+        <DiscordLogoWhite width={200} />
+      </a>
+      <a className='mt-5' href='https://slack.everydayproductive.com'>
+        <SlackLogoWhite width={200} />
+      </a>
     </div>
   )
 }
