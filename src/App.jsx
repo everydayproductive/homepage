dfbb3a72183c2d0ed30a8cd904b9bfa67af44a87
Marcin Czenko <marcin.czenko@pm.me>
2022-11-09 00:14:23 +0000

    More responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import { ReactComponent as SlackLogoWhite } from '~/assets/Slack_RGB_White.svg'
 const App = () => {
   return (
     <div className='flex w-screen flex-col items-center md:justify-center'>
-      <div className='mb-14 flex w-screen items-center md:justify-center'>
+      <div className='mb-8 flex w-screen items-center sm:mb-14 md:justify-center'>
         <div className='w-full max-w-2xl md:w-4/5'>
-          <div className='flex justify-center'>
-            <div className='flex items-end pr-5 pb-5'>
-              <EPLogo width={130} />
+          <div className='mt-5 flex flex-col items-center justify-center sm:mt-0 sm:flex-row sm:items-end'>
+            <div className='flex w-28 items-end pr-5 pb-5 sm:w-36'>
+              <EPLogo width='100%' />
             </div>
             <div className='flex items-end justify-end'>
-              <div className='w-min-content whitespace-pre-line bg-[#459ED0] pt-28 pl-10 pr-3 pb-5 font-roboto-mono text-5xl font-light text-white'>
-                {'Creative\nConstraints'}
+              <div className='w-min-content flex flex-col items-center bg-[#459ED0] p-5 font-roboto-mono text-3xl font-light text-white sm:items-start sm:pt-28 sm:pl-10 sm:pr-3 sm:pb-5 sm:text-5xl'>
+                <p>Creative</p>
+                <p>Constraints</p>
               </div>
             </div>
           </div>
@@ -31,57 +32,65 @@ const App = () => {
         ultricies ipsum egestas eu.
       </div>
       <div className='mt-16 flex w-full items-center justify-center bg-[#459ED0]'>
-        <div className='flex w-full max-w-2xl overflow-y-visible px-5 md:px-0'>
-          <div className='flex w-3/4 justify-between font-roboto-mono text-xs font-light text-white md:w-4/5'>
-            <div className='flex flex-col py-2 pr-3'>
-              <h1>Everyday Productive</h1>
-              <h2 className='mt-2 text-[0.5rem] font-bold'>
-                Visiting address:
-              </h2>
-              <p className='whitespace-pre-line text-[0.5rem] leading-tight'>
-                {`Microlab
-                  Kastanjelaan 400
-                  5616LZ Eindhoven`}
-              </p>
-            </div>
-            <div className='flex flex-col py-2 pr-3'>
-              <h1>Contact</h1>
-              <h2 className='mt-2 text-[0.5rem]'>
-                info@everydayproductive.com
-              </h2>
-              <div className='flex items-center'>
-                <a
-                  className='mr-2 block'
-                  href='https://discord.com/invite/Sq5bgwpwWs'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <DiscordLogoWhite width={60} height='auto' />
-                </a>
-                <a
-                  className='block'
-                  href='https://join.slack.com/t/everydayproductive/shared_invite/zt-1j8ilbdcj-bZawEy2UfTz6FTrN5xYowQ'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <SlackLogoWhite width={50} height='auto' />
-                </a>
+        <div className='w-full max-w-2xl overflow-x-visible px-5 md:px-0'>
+          <div className='flex w-full flex-col sm:flex-row lg:w-[120%]'>
+            <div className='flex w-full font-roboto-mono text-[0.5rem] font-light text-white sm:w-3/4 sm:justify-between md:w-4/5 md:text-xs'>
+              <div className='flex min-w-min flex-col py-2 pr-3'>
+                <h1 className='whitespace-nowrap text-xs sm:text-sm'>
+                  Everyday Productive
+                </h1>
+                <h2 className='mt-2 mb-1 font-bold'>Visiting address:</h2>
+                <p className='whitespace-pre-line leading-tight'>
+                  {`Microlab
+                    Kastanjelaan 400
+                    5616LZ Eindhoven`}
+                </p>
+                <div className='flex flex-col py-3 sm:hidden'>
+                  <p className='whitespace-nowrap'>KVK: 51982277</p>
+                  <p className='whitespace-nowrap'>BTW: NL002447038B96</p>
+                  <p className='whitespace-nowrap sm:mt-4'>
+                    Koningskinderen 29
+                  </p>
+                  <p className='whitespace-nowrap'>5629KC Eindhoven</p>
+                </div>
+              </div>
+              <div className='ml-10 flex flex-col py-2 pr-3 sm:ml-0'>
+                <h1 className='text-xs sm:text-sm'>Contact</h1>
+                <h2 className='mt-2'>info@everydayproductive.com</h2>
+                <div className='flex flex-col sm:flex-row sm:items-center'>
+                  <a
+                    className='block sm:mb-5 sm:mr-2'
+                    href='https://discord.com/invite/Sq5bgwpwWs'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='w-20 sm:w-32 sm:pr-5'>
+                      <DiscordLogoWhite width='100%' height={40} />
+                    </div>
+                  </a>
+                  <a
+                    className='mb-5 block'
+                    href='https://join.slack.com/t/everydayproductive/shared_invite/zt-1j8ilbdcj-bZawEy2UfTz6FTrN5xYowQ'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='w-24 pr-5 sm:w-28'>
+                      <SlackLogoWhite width='100%' height={40} />
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className='hidden items-center justify-end bg-white px-2 lg:flex'>
+                <EPLogo width={80} />
               </div>
             </div>
-            <div className='invisible flex items-center justify-end bg-white px-2 md:visible'>
-              <EPLogo width={80} />
-            </div>
-          </div>
-          <div className='ml-5 flex font-roboto-mono text-xs font-light text-white'>
-            <div className='flex flex-col py-3'>
-              <p className='whitespace-pre-line text-[0.5rem] leading-tight'>
-                {`KVK: 51982277
-                  BTW: NL002447038B96`}
-              </p>
-              <p className='mt-4 whitespace-pre-line text-[0.5rem] leading-tight'>
-                {`Koningskinderen 29
-                  5629KC Eindhoven`}
-              </p>
+            <div className='hidden font-roboto-mono text-[0.5rem] font-light text-white sm:ml-5 sm:flex sm:text-xs'>
+              <div className='flex flex-col py-3'>
+                <p className='whitespace-nowrap'>KVK: 51982277</p>
+                <p className='whitespace-nowrap'>BTW: NL002447038B96</p>
+                <p className='whitespace-nowrap sm:mt-4'>Koningskinderen 29</p>
+                <p className='whitespace-nowrap'>5629KC Eindhoven</p>
+              </div>
             </div>
           </div>
         </div>
