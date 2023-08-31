6cfa560eb5ab3cf90e740f9aa5219e6cd298eca3
Marcin Czenko <marcin.czenko@pm.me>
2023-08-31 03:58:30 +0200

    Updates formatting of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as SlackLogoWhite } from '~/assets/Slack_RGB_White.svg'
 
 const Footer = () => (
   <div className='mt-10 flex w-full items-center justify-center bg-[#459ED0] lg:mt-16'>
-    <div className='w-full max-w-2xl overflow-x-visible px-5 md:px-0'>
-      <div className='flex w-full flex-row lg:w-[110%] xl:w-[120%]'>
+    <div className='w-[90%] overflow-x-visible px-5 md:px-0'>
+      <div className='flex w-full flex-row'>
         <div className='flex w-3/4 flex-col font-roboto-mono text-xs font-light text-white sm:flex-row sm:justify-between md:w-4/5 md:text-xs'>
           <div className='flex min-w-min flex-col px-2 py-2 pr-3'>
             <h1 className='whitespace-nowrap text-xs sm:text-sm'>
@@ -22,12 +22,11 @@ const Footer = () => (
               <p className='whitespace-nowrap'>BTW: NL002447038B96</p>
               <p className='whitespace-nowrap sm:mt-4'>Koningskinderen 29</p>
               <p className='whitespace-nowrap'>5629KC Eindhoven</p>
-              {/* <p className='whitespace-nowrap'>info@everydayproductive.com</p> */}
             </div>
           </div>
-          <div className='ml-0 flex-col px-2 py-2 pr-3 sm:ml-10 sm:flex'>
+          <div className='ml-0 flex-col px-2 py-2 pr-10 sm:ml-10 sm:flex'>
             <h1 className='text-xs sm:text-sm'>Contact</h1>
-            <div className='flex flex-col md:flex-row md:items-center'>
+            <div className=':items-center flex flex-col lg:flex-row'>
               <a
                 className='block sm:mb-0 sm:mr-2'
                 href='https://discord.com/invite/Sq5bgwpwWs'
@@ -52,7 +51,7 @@ const Footer = () => (
             <h2 className='mb-2 mt-2'>info@everydayproductive.com</h2>
           </div>
           <div className='hidden bg-white px-2 lg:flex lg:items-center lg:justify-end'>
-            <EPLogo width={80} />
+            <EPLogo width={80} className='my-6' />
           </div>
         </div>
         <div className='hidden pr-2 font-roboto-mono font-light text-white sm:text-xs md:flex lg:ml-5'>
