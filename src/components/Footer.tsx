459a429f367f562ed5f00c2fb3a54db3878a045f
Marcin Czenko <marcin.czenko@pm.me>
2025-10-20 20:55:22 +0200

    Updates BTW and KvK in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
         <div className='flex w-3/4 flex-col font-roboto-mono text-xs font-light text-white sm:flex-row sm:justify-between md:w-4/5 md:text-xs'>
           <div className='flex min-w-min flex-col px-2 py-2 pr-3'>
             <h1 className='whitespace-nowrap text-xs sm:text-sm'>
-              Everyday Productive
+              Everyday Productive B.V.
             </h1>
             <h2 className='mb-1 mt-2 font-bold'>Visiting address:</h2>
             <p className='whitespace-pre-line leading-tight'>
@@ -56,8 +56,8 @@ const Footer = () => (
         </div>
         <div className='hidden pr-2 font-roboto-mono font-light text-white sm:text-xs md:flex lg:ml-5'>
           <div className='flex flex-col py-3'>
-            <p className='whitespace-nowrap'>KVK: 51982277</p>
-            <p className='whitespace-nowrap'>BTW: NL002447038B96</p>
+            <p className='whitespace-nowrap'>KVK: 97639958</p>
+            <p className='whitespace-nowrap'>BTW: NL868151737B01</p>
             <p className='whitespace-nowrap sm:mt-4'>Koningskinderen 29</p>
             <p className='whitespace-nowrap'>5629KC Eindhoven</p>
           </div>
